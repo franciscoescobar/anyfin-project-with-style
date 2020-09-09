198549a5edacdf534d0fcfbefffde8483670d181
Francisco Escobar Sabio <franciscoescobar@users.noreply.github.com>
2020-09-08 21:42:17 -0300

    Updates

---
+++ precache-manifest.082d7b24c12909d7f7eacefc35995ef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd25f16cfa47afa859bf40735b6636e",
+    "url": "/anyfin-project-with-style/index.html"
+  },
+  {
+    "revision": "714f190b9f24b50d4f52",
+    "url": "/anyfin-project-with-style/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8bbde6578110f24d1448",
+    "url": "/anyfin-project-with-style/static/js/2.5c1c4c34.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/anyfin-project-with-style/static/js/2.5c1c4c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714f190b9f24b50d4f52",
+    "url": "/anyfin-project-with-style/static/js/main.cd2c387b.chunk.js"
+  },
+  {
+    "revision": "1a0636cf53dfa15be843",
+    "url": "/anyfin-project-with-style/static/js/runtime-main.7387e03b.js"
+  }
+]);
